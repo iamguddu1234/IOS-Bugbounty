17a2cd6d04deee5d8fd72749cd1e5ee001398508
Akshay Bhamse <iamguddu37@gmail.com>
2024-06-03 12:59:01 +0530

    Initial Commit

---
+++ BugBounty/BugBountyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BugBountyApp.swift
+//  BugBounty
+//
+//  Created by Akshay Bhasme on 03/06/24.
+//
+
+import SwiftUI
+
+@main
+struct BugBountyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
